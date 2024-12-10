e403a8813bcc9fb5db62852f766d885ccc37a843
stsdc <6031763+stsdc@users.noreply.github.com>
2024-12-10 21:09:02 +0100

    Add initial stripe feature

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,5 +1,6 @@
 from typing import Sequence, Annotated
 from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy import select, delete
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,6 +9,12 @@ from app.db.models import Repositories, Users, Issues
 from app.db.schemas import repositories as repos_schema
 from app.deps import get_current_user
 
+
+import stripe
+# This test secret API key is a placeholder. Don't include personal details in requests with this key.
+# To see your test secret API key embedded in code samples, sign in to your Stripe account.
+# You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
+stripe.api_key = ''
 
 auth_user_dependency = Annotated[Users, Depends(get_current_user)]
 
@@ -48,3 +55,31 @@ async def get_post(
     return templates.TemplateResponse(
         name="repository.html", request=request, context={"repository": repository, "issues": issues}
     )
+
+@router.post('/create-checkout-session')
+async def create_checkout_session(request: Request, ):
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1QU5fsHoNPk03hr4E2n5ztVe',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=str(request.base_url) + '/success.html',
+            cancel_url=str(request.base_url) + '/cancel.html',
+            custom_fields=[
+                {
+                    "key": "issue",
+                    "label": {"type": "custom", "custom": "Repository and Issue number"},
+                    "type": "text",
+                    "text": {"default_value": "some-repo#123"},
+                }
+            ]
+        )
+    except Exception as e:
+        return str(e)
+
+    return RedirectResponse(checkout_session.url, status_code=status.HTTP_303_SEE_OTHER)
