21ef075611766f829721b054da916eda527e62d9
stsdc <6031763+stsdc@users.noreply.github.com>
2025-01-30 21:25:43 +0100

    Fix some deprecations

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
+"""Utils."""
 from os import getenv
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from jose import jwt
 from passlib.context import CryptContext
 
@@ -15,30 +16,58 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password, hashed_password):
+    """
+    Verify that a plain text password matches a hashed password.
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
+    """
+    Hashes a given password using the configured password context.
+    """
     return pwd_context.hash(password)
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Creates a JSON Web Token (JWT) for the given data with an optional expiration time.
+
+    Args:
+        data (dict): The data to encode in the JWT.
+        expires_delta (timedelta | None, optional): The time duration after which the token will expire. 
+            If not provided, the token will expire in 15 minutes.
+
+    Returns:
+        str: The encoded JWT as a string.
+    """
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(UTC) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(UTC) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
 def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Creates a refresh token with the given data and expiration time.
+
+    Args:
+        data (dict): The data to include in the token payload.
+        expires_delta (timedelta | None, optional): The time duration after which the token will expire. 
+            If not provided, the token will expire in 15 minutes.
+
+    Returns:
+        str: The encoded JWT refresh token.
+    """
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(UTC) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(UTC) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
