1733a2f01c942df9b2c4d6faf42da0354aa2df9d
stsdc <6031763+stsdc@users.noreply.github.com>
2025-01-31 14:46:45 +0100

    Fix repositories API

--- app/routers/repositories.py
+++ app/routers/repositories.py
@@ -1,24 +1,22 @@
-from typing import Sequence, Annotated
-from fastapi import APIRouter, Depends, HTTPException
+"""Repositories API."""
+
+from typing import Sequence
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import sessions
-from app.db.models import Repositories, Users
+from app.db.models import Repositories
 from app.db.schemas import repositories as repos_schema
-from app.deps import get_current_user
-
-
-auth_user_dependency = Annotated[Users, Depends(get_current_user)]
 
 router = APIRouter(prefix="/api/repositories", tags=["api", "repositories"])
 
 
-@router.get("/")
-async def get_repositories(db: AsyncSession = Depends(sessions.get_async_session)) -> Sequence[repos_schema.Repositories]:
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_repositories(
+    db: AsyncSession = Depends(sessions.get_async_session),
+) -> Sequence[repos_schema.Repositories]:
+    """Get all repositories."""
     q = select(Repositories)
     result = await db.execute(q)
     repos = result.scalars().all()
-
-    if not repos:
-        raise HTTPException(status_code=404, detail="No repos found")
     return repos
